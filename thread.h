9323d7dd1f5fb7c19527abfdfdec1a63629c6a1c
Carlos <carla.cesarean@gmail.com>
2017-01-19 21:53:51 +0200

    second mesage

--- thread.h
+++ thread.h
@@ -92,7 +92,7 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
-
+    struct lock *wait_lock;				/*The lock in which the thread has to wait upon*/
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -138,4 +138,5 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
+bool priority_compare(const struct list_elem *x,const struct list_elem *y, void *aux UNUSED);
 #endif /* threads/thread.h */
